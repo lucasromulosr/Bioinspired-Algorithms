f963c5875dd657c8bdc556eef896a52486598399
srlucasromulo <lucas.romulo.sr@gmail.com>
2022-04-05 22:24:38 -0300

    writing AG

---
+++ AG/main.py
@@ -0,0 +1,106 @@
+import math
+
+import numpy as np
+from typing import List
+
+n = 2
+precision = 6
+npop = 20
+ngen = 10
+xmin = -2
+xmax = 2
+
+
+class Solution:
+
+    # generate a new random solution
+    def __init__(self):
+        self.fitness = None
+        self.x = []
+        for _ in range(n):
+            self.x.append(np.random.randint(2, size=precision))
+
+    def convert_x(self):
+        x = []
+        for i in range(n):
+            value = xmin + ((xmax - xmin)/(pow(2, n) - 1)) * int_bin(self.x[i][:])
+            x.append(value)
+        return x
+
+    def fitness_function(self):
+        self.fitness = f(self.convert_x())
+
+    def __str__(self):
+        string = f'{np.array2string(self.x[0])} {np.array2string(self.x[1])} {str(self.fitness)}'
+        return string
+
+
+# OBJECTIVE FUNCTION
+def f(x: list):
+    sum1 = 0
+    sum2 = 0
+    for i in range(n):
+        sum1 += pow(x[i], 2)
+        sum2 += math.cos(2 * math.pi * x[i])
+
+    fsum1 = -0.2 * math.sqrt(sum1/n)
+    fsum2 = sum2/n
+
+    return -20 * math.exp(fsum1) - math.exp(fsum2) + 20 + math.e
+
+
+def int_bin(x: list):
+    return int("".join(str(i) for i in x), 2)
+
+
+def generate_initial_population():
+    population = []
+    for _ in range(npop):
+        population.append(Solution())
+    return population
+
+
+def population_fitness(population: List[Solution]):
+    for s in population:
+        s.fitness_function()
+
+
+def generate_parents(population: List[Solution]):
+    parents = []
+
+    for _ in range(npop):
+        p1 = np.random.randint(npop)
+        p2 = np.random.randint(npop)
+        while p1 == p2:
+            p2 = np.random.randint(npop)
+
+        parents.append(population[p1]) if population[p1].fitness > population[p2].fitness \
+            else parents.append(population[p2])
+
+    return parents
+
+
+def crossover(parents: List[Solution]):
+    population = []
+
+    # parents.sort(key=lambda x: x.fitness) # sort parents
+
+    pass
+
+
+if __name__ == "__main__":
+
+    population = generate_initial_population()
+
+    # generations loop
+    # for _ in range(ngen):
+    #     pass
+
+    population_fitness(population)
+
+    parents = generate_parents(population)
+
+    # population = crossover(parents)
+
+    for i in range(npop):
+        print(parents[i])
---
+++ AG/solution.py
@@ -0,0 +1,5 @@
+class Solution:
+
+    def __init__(self):
+        pass
+
